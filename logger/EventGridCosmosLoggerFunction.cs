756f56183d036b15774da6632d7c2e555bd949aa
nandanmathure@gmail.com <nandanmathure@gmail.com>
2021-06-02 22:17:40 -0400

    Initial Build

---
+++ logger/EventGridCosmosLoggerFunction.cs
@@ -0,0 +1,48 @@
+#region LICENSE
+// Copyright(c) 2021 Nandan Mathure
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+#endregion
+
+// Default URL for triggering event grid function in the local environment.
+// http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
+
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.EventGrid.Models;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace logger
+{
+    public static class EventGridCosmosLoggerFunction
+    {
+        [FunctionName("EventHandler")]
+        public static async Task EventHandler(
+            [EventGridTrigger]EventGridEvent eventGridEvent,
+            [CosmosDB(
+                databaseName: "ToDoItems",
+                collectionName: "Items",
+                ConnectionStringSetting = "CosmosDBConnection")]
+                IAsyncCollector<EventGridEvent> eventGridEvents)
+        {
+            await eventGridEvents.AddAsync(eventGridEvent);
+        }
+    }
+}
